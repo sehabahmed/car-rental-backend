894e975410480d647ae826e23fb1d9b0d62499e6
Shihab585203 <freelancersehab1@gmail.com>
2025-02-16 22:26:22 +0100

    booking data updated successfully

--- src/app/modules/booking/booking.route.ts
+++ src/app/modules/booking/booking.route.ts
@@ -2,16 +2,19 @@ import { Router } from 'express';
 import validateRequest from '../utils/validateRequest';
 import { createBookingValidationSchema } from './booking.validation';
 import { BookingControlers } from './booking.controller';
+import auth from '../../middleware/auth';
+import { USER_ROLE } from '../users/user.constant';
 
 const router = Router();
 
 router.post(
   '/create-booking',
+  auth(USER_ROLE.user),
   validateRequest(createBookingValidationSchema),
   BookingControlers.createBooking,
 );
 
-router.get('/', BookingControlers.getAllBooking);
+router.get('/', auth(USER_ROLE.admin), BookingControlers.getAllBooking);
 
 router.get('/:id', BookingControlers.getSingleBookingFromDB);
 
