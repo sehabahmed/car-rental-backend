74c0b88a1e04a6e396ff5950b0bd158232497e61
Shihab585203 <freelancersehab1@gmail.com>
2025-02-22 22:36:53 +0100

    added handle validation and zod error

--- src/app/errors/handleDuplicateError.ts
+++ src/app/errors/handleDuplicateError.ts
@@ -1,1 +1,25 @@
-const handleDuplicateError = (err: any)
+import { TErrorSources, TGenericErrorResponse } from '../interface/error';
+
+const handleDuplicateError = (err: any): TGenericErrorResponse => {
+  const match = err.message.match(/"([^"])"/);
+
+  const extractedMessage = match && match[1];
+
+  const errorSources: TErrorSources = [
+    {
+      path: '',
+      message: `${extractedMessage} is already exists`,
+    },
+  ];
+
+  const statusCode = 400;
+
+  return {
+    statusCode,
+    message: 'Duplicate Error',
+    errorSources,
+  };
+};
+
+
+export default handleDuplicateError;
